589d8bd3eb0f18105bd9959a6b85bdc4fb75897d
Tertius Roach <tertius.roach@gmail.com>
2022-09-19 11:42:15 +0200

    January & February Reveal Built

--- src/code/events/index-overlay.ts
+++ src/code/events/index-overlay.ts
@@ -4,9 +4,31 @@ export namespace IndexOverlay {
       case 'default-overlay':
         break;
       case 'sidebar-overlay':
+        let displayOverlay = () => {};
+        $('#index-sidebar #january-button a h1').on('mouseenter', () => {
+          IndexOverlay.displayOverlay(pageName, 'january');
+          // $(`.${pageName} #january`).css('display', 'grid');
+        });
+        $('#index-sidebar #february-button a h1').on('mouseenter', () => {
+          IndexOverlay.displayOverlay(pageName, 'february');
+        });
         console.log(`--${pageName} Loaded`);
         break;
     }
     //--► console.log(`--${pageName} Loaded`); ◄--//
+  }
+
+  export function displayOverlay(
+    pageName: String,
+    display: 'january' | 'february' | 'march' | 'april' | 'may' | 'june' | 'july' | 'august' | 'september' | 'october' | 'november' | 'december'
+  ) {
+    var months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+    for (let i = 0; i < months.length; i++) {
+      if (months[i] === display) {
+        $(`.${pageName} #${display}`).css('display', 'grid');
+      } else {
+        $(`.${pageName} #${months[i]}`).css('display', 'none');
+      }
+    }
   }
 }
--- src/code/events/index-sidebar.ts
+++ src/code/events/index-sidebar.ts
@@ -2,6 +2,15 @@ export namespace IndexSidebar {
   export function eventsFor(pageName: String | 'default-sidebar') {
     switch (pageName) {
       case 'default-sidebar':
+        const defaultSidebar = document.querySelector(`.${pageName}`);
+
+        const januaryButton = document.getElementById('january-button');
+
+        const indexOverlay = document.getElementById('index-overlay');
+        $(`.${pageName}`).on('mouseenter', () => {
+          indexOverlay.style.display = 'grid';
+        });
+        console.log(`--${pageName} Loaded`);
         break;
     }
     //--► console.log(`--${pageName} Loaded`); ◄--//
