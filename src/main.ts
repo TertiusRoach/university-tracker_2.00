d237ffb29124aad76efaa6de2ad0adba45ea2230
Tertius Roach <tertius.roach@gmail.com>
2022-11-20 19:25:45 +0200

    Note Changes

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,12 @@ import { Uitsendings } from './code/uitsendings';
 import { Akademici } from './code/akademici';
 import { Index } from './code/index';
 namespace Main {
-  //==================================//
-  //--|▼| Link Coil monetization services |▼|--//
+  //--|▼| Add monetization services |▼|--//
   const link = document.querySelector('link[rel="monetization"]');
   link.addEventListener('monetization', () => {
-    // console.log(monetization);
     console.log('Yippie! Monetization is on.');
   });
-  //==================================//
+
   //--|▼| Finds the location of the *.html file |▼|--//
   const page: String = window.location.href.split('/').pop().split('.')[0];
   switch (page) {
